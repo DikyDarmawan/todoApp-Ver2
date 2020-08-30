f2db24cf5a0d1c0d95d598b4d4e8340ab0d807c0
Diky Darmawan <darmawandiky0@gmail.com>
2020-08-30 12:20:11 +0700

    Updates

---
+++ precache-manifest.00bd8a0a1d24db21c9036adc40d5df34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36cbb98fa3ee34c5728e6490b9dbe9e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bada75cd9388181e5b3",
+    "url": "/static/css/main.a92be6b9.chunk.css"
+  },
+  {
+    "revision": "72953c6cc522cb3487d0",
+    "url": "/static/js/2.1367d13f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1367d13f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bada75cd9388181e5b3",
+    "url": "/static/js/main.d0d7a451.chunk.js"
+  },
+  {
+    "revision": "10545e6b10910a0b9f12",
+    "url": "/static/js/runtime-main.025be44c.js"
+  }
+]);
